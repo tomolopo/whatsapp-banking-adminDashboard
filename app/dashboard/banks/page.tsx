cd1025e84c39c537274c814abb787db17c3c683e
tomolopo <taiwo.omolopo@infobip.com>
2026-03-13 13:14:06 +0100

    one more adjustment

--- app/dashboard/banks/page.tsx
+++ app/dashboard/banks/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { fetchBanks } from "@/lib/api"
+import { fetchBanks } from "../../../lib/api"
 
 export default function Banks(){
 
